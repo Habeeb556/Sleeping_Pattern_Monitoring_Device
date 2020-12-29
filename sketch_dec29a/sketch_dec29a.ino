3b257863486a12058e6629b8478ab39344f09afc
Habeeb556 <ahmed556@std.mans.edu.eg>
2020-12-29 23:47:59 +0200

    Add all my files

--- sketch_dec29a/sketch_dec29a.ino
+++ sketch_dec29a/sketch_dec29a.ino
@@ -26,32 +26,38 @@ x = mpu6050.getGyroX();
 y = mpu6050.getGyroY();
 z = mpu6050.getGyroZ();
 
+//check condition, If our hand is not moving or in rest.
 if (activate == 0){ // first sleep confirmation
   if ((x<=20 || x>=-20) && (y<=20 || y>=-20) && (z<=20 || z>=-20)) {
      sleep_timer_start = millis()/1000-sleep_timer_end;
-      if (sleep_timer_start == 10){
+      if (sleep_timer_start == 30){
         activate = 1;}
 }
+//Check if within 30 seconds our hand move then it will gain start the timer.
   if ((x>=20 || x<=-20) || (y>=20 || y<=-20) || (z>=20 || z<=-20)){
     sleep_timer_end =(millis()/1000); }
 }
 
 if (activate == 1){ // sleeping mode
 light_sleep = (millis()/1000)-sleep_timer_end;
-
+//When sleep cycle starts after 30 seconds, we go to deep sleep.
   if (interrupt == 0){
-    if (light_sleep >= 20){
-      if (light_sleep - interrupt_sleep_timer >= 10){
-        deep_sleep = light_sleep - interrupt_for_deep_sleep;
+    if (light_sleep >= 60){
+      if (interrupt_for_deep_sleep > 60){
+        if (light_sleep - interrupt_sleep_timer >= 10){
+          deep_sleep = light_sleep - interrupt_for_deep_sleep;
         }
       }
     }
+  }
 light_sleep = light_sleep - deep_sleep;
 Serial.print("light_sleep: ");
 Serial.println(light_sleep);
 Serial.print("deep_sleep: ");
 Serial.println(deep_sleep);
 
+//If you are in sleeping mode then your hand movement doesn’t make light sleep equal to zero. 
+//Whenever your hand move it will count interrupt.
            if ((x>=20 || x<=-20) || (y>=20 || y<=-20) || (z>=20 || z<=-20)){
             interrupt_sleep_timer = (millis()/1000)-sleep_timer_end; 
             interrupt_for_deep_sleep = light_sleep;
@@ -63,15 +69,15 @@ Serial.println(deep_sleep);
             delay(2500);}
 
 
-
-if ((millis()/1000)- sleep_timer_end -interrupt_sleep_timer > 10) {
+//if your last interruption was 5 minutes ago then it will make interrupt is equal to zero.
+if ((millis()/1000)- sleep_timer_end -interrupt_sleep_timer > 300) {
 interrupt =0; 
 }
 
 if ((millis()/1000)- sleep_timer_end - interrupt_sleep_timer <= 10){
 if (interrupt >=5){
 sleep_timer_end =(millis()/1000);
-if (light_sleep >= 30){ // second sleep confirmation
+if (light_sleep >= 120){ // second sleep confirmation
 total_light_sleep = total_light_sleep + light_sleep;
 total_deep_sleep = total_deep_sleep + deep_sleep;
 total_sleep = total_light_sleep + total_deep_sleep; }
